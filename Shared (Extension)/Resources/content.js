ab94621382d8ebd97a61792d3e4879fa4625966f
gokulnair2001 <gokulnair.2001@gmail.com>
2022-02-04 17:45:17 +0530

    Meta data query added

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,6 +1,6 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+let titleContent = document.querySelector('meta[property="og:title"]')?.content;
+let descriptionContent = document.querySelector('meta[property="og:description]')?.content;
+let imageURL = document.querySelector('meta[property="og:image"]')?.content;
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
