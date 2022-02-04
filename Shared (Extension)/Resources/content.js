0648a5349b3ceb6a1f3294aac0c7a5fd391d3ca0
gokulnair2001 <gokulnair.2001@gmail.com>
2022-02-04 21:50:07 +0530

    CSS HTML code integrated

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,16 @@
 let titleContent = document.querySelector('meta[property="og:title"]')?.content;
-let descriptionContent = document.querySelector('meta[property="og:description]')?.content;
+let descriptionContent = document.querySelector('meta[property="og:description"]')?.content;
 let imageURL = document.querySelector('meta[property="og:image"]')?.content;
+let siteURL = document.querySelector('meta[property="og:url"]')?.content;
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    if (request.update != "please")
+        return;
+    
+    sendResponse({
+        title: titleContent,
+        description: descriptionContent,
+        url: siteURL,
+        image: imageURL
+    });
 });
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,13 @@
-console.log("Hello World!", browser);
+let titleElement = document.getElementById('title');
+let descriptionElement = document.getElementById('description');
+let imageElement = document.getElementById('image');
+let urlElement = document.getElementById('url');
+
+let tabs = await browser.tabs.query({active: true, currentWindow: true});
+let response = await browser.tabs.sendMessage(tabs[0].id, {update: "please"});
+
+titleElement.textContent = response?.title;
+descriptionElement.textContent = response?.description;
+urlElement.textContent = response?.url;
+if (response?.image !== undefined)
+        imageElement.src = response?.image;
